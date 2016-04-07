d2971ff4e640983b986e268506222a8999a7a8b6
Augusto Dias <augustovicentefernandes@gmail.com>
2016-04-07 11:22:52 -0300

    Teste alteração

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@ int main(){
 	int year=2016, x;
 	printf ("Seu idade: ");
 	scanf ("%d", &x);
-	printf ("\nVocê nasceu em: %d\n", year-x);
+	printf ("\nVocê nasceu no ano de: %d\n", year-x);
 	
 return 0;
 }
